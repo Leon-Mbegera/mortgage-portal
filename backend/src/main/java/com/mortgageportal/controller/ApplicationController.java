3a37bc4149f9740648b851bb61a436661dfa0975
Leon-Mbegera <mbegeraleon89@gmail.com>
2026-09-02 18:01:25 +0300

    Add GET endpoint to retrieve application details by ID

--- backend/src/main/java/com/mortgageportal/controller/ApplicationController.java
+++ backend/src/main/java/com/mortgageportal/controller/ApplicationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -35,5 +36,10 @@ public class ApplicationController {
   @GetMapping
   public ResponseEntity<List<ApplicationResponse>> list(@AuthenticationPrincipal UserPrincipal principal) {
     return ResponseEntity.ok(applicationService.listForCurrentUser(principal.getUser()));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ApplicationResponse> getById(@PathVariable Long id, @AuthenticationPrincipal UserPrincipal principal) {
+    return ResponseEntity.ok(applicationService.getById(id, principal.getUser()));
   }
 }
